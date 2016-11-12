13199652235a46e76d70ce62d57ca65da360d014
Alwin Arrasyid <alwin.ridd@gmail.com>
2016-11-12 09:12:52 +0700

    Added request object validation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 'use strict';
 
-function parser() {
+const _ = require('lodash');
 
+function validate(object, required) {
+  return _.filter(required, _.has.bind(null, object)).length === required.length;
+}
+
+function parser(request) {
+  if (!_.isObject(request) || _.isArray(request)) {
+    throw new TypeError('Passed argument is not a valid request object');
+  }
+
+  const requiredFields = ['draw', 'columns', 'search', 'order', '_', 'start', 'length'];
+
+  if (!validate(request, requiredFields)) {
+    throw new TypeError(`${_.difference(requiredFields, _.keys(request))} are required`);
+  }
 }
 
 module.exports = parser;
--- test/main.js
+++ test/main.js
@@ -10,5 +10,48 @@ describe('Datatable parser module', () => {
   it('Should export a function', () => {
     expect(parser).to.be.a('function');
   });
+
+  it('Should throws if it is passed invalid argument type', () => {
+    const invalidArgs = [
+      'a string',
+      true,
+      1234,
+      [],
+    ];
+
+    invalidArgs.forEach(arg => expect(parser.bind(null, arg)).to.throw(TypeError));
+  });
+
+  it('Should trhows if it is passed invalid request object', () => {
+    const validParam = {
+      draw: '1',
+      columns: [],
+      search: {},
+      start: '0',
+      length: '10',
+      order: [],
+      _: '12312415',
+    };
+
+    const params = [
+      {
+        draw: '1',
+        columns: [],
+      },
+      {
+        draw: '1',
+        order: [],
+      },
+      {
+        columns: [],
+        start: '0',
+        length: '10',
+      },
+      {},
+    ];
+
+    params.forEach(arg => expect(parser.bind(null, arg)).to.throw(TypeError));
+    expect(parser.bind(null, validParam)).not.to.throw(TypeError);
+  });
 });
 
